d61edb41e65a9f83207a63689f1db7f8adba0657
Franco De Leon <franco@carbonodev.com>
2017-11-21 22:23:50 -0600

    Add laravel mix

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -37,17 +37,13 @@ if (token) {
     console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
 }
 
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
- */
+window.toastr = require('toastr');
 
-// import Echo from 'laravel-echo'
-
-// window.Pusher = require('pusher-js');
-
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: 'your-pusher-key'
-// });
+window.toastr.options = {
+    "closeButton": true,
+    "positionClass": "toast-top-right",
+    "showDuration": "300",
+    "hideDuration": "1000",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+};
